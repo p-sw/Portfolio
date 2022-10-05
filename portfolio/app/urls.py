6430dd28d988064228247d928f2d9f1dacbdcea6
sserve-kr <personal@sserve.me>
2022-10-05 21:33:58 +0900

    Added basic navigation

--- portfolio/app/urls.py
+++ portfolio/app/urls.py
@@ -2,6 +2,8 @@ app_name = 'app'
 
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('app.urls', namespace='app')),
+    path('', include('app.urls', namespace='app')),
 ]
