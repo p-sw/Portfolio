1161fb923e2172a82388aa8937e714a28114382f
sserve-kr <personal@sserve.me>
2022-10-09 16:47:13 +0900

    Added hangul animation

--- portfolio/app/static/app/js/index.js
+++ portfolio/app/static/app/js/index.js
@@ -1,13 +1,37 @@
 welcome_text = {
     object: document.getElementById("welcome-title"),
-    text: "Welcome to my portfolio!",
+
+    text: "안녕하세요. \n웹 개발자 서브입니다.",
+
+    get_flat: function() {
+        return [].concat(...Hangul.disassemble(this.text));
+    },
+
+    get_index: function() {
+        this.max_index = this.get_flat().length;
+    },
+
     index: 0,
-    speed: 100,
-    write: function() {
-        if (this.index < this.text.length) {
-            this.object.innerHTML += this.text.charAt(this.index);
+
+    speed: 50,
+    special_speeds: {
+        " ": 150,
+        "\n": 200,
+    },
+
+    start: function() {
+        if (this.index <= this.max_index) {
+            this.object.innerText = Hangul.assemble(this.get_flat().slice(undefined, this.index));
             this.index++;
-            setTimeout(this.write.bind(this), this.speed);
+            current_char = this.get_flat()[this.index - 1];
+            if ("\n" === current_char) {
+                this.object.innerHTML += "<br>";
+            }
+            if (current_char in this.special_speeds) {
+                setTimeout(this.start.bind(this), this.special_speeds[current_char])
+            } else {
+                setTimeout(this.start.bind(this), this.speed);
+            }
         }
     }
 }
