639eb69a6bf67ad03bfbf3cfeef593bbd74bd590
sserve-kr <personal@sserve.me>
2022-10-09 17:37:27 +0900

    Added animation on sub-nav buttons

--- portfolio/app/static/app/js/index.js
+++ portfolio/app/static/app/js/index.js
@@ -1,3 +1,18 @@
+sub_nav = {
+    buttons: document.querySelector("nav.sub-nav").children,
+    visible_delay: 400,
+    i: 0,
+    buttons_visible: function() {
+        if (this.i<=1) {
+            this.buttons[this.i].classList.add("visible");
+            this.i++;
+        } else {
+            return;
+        }
+        setTimeout(this.buttons_visible.bind(this), this.visible_delay);
+    }
+}
+
 welcome_text = {
     object: document.getElementById("welcome-title"),
 
@@ -32,6 +47,8 @@ welcome_text = {
             } else {
                 setTimeout(this.start.bind(this), this.speed);
             }
+        } else {
+            sub_nav.buttons_visible();
         }
     }
 }
