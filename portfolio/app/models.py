b41fcf7575dcb0ab864600a56a2c7f6e3d24b16f
sserve-kr <personal@sserve.me>
2022-10-11 18:35:49 +0900

    Added project model

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ProjectDependencies(models.Model):
+    name = models.CharField(max_length=20, unique=True, primary_key=True)
+    icon = models.ImageField(upload_to='icons')
+
+class Projects(models.Model):
+    title = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=100)
+    dependencies = models.ManyToManyField(ProjectDependencies)
+    start_commit = models.DateField()
+    last_commit = models.DateField()
+    finished = models.BooleanField(default=False)
