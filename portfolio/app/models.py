16ee3c7241b91805637603cf576e7c98ad87d266
sserve-kr <personal@sserve.me>
2022-10-12 17:03:18 +0900

    Added __str__ method in models

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class ProjectDependencies(models.Model):
     name = models.CharField(max_length=20, unique=True, primary_key=True)
     icon = models.ImageField(upload_to='icons')
+    
+    def __str__(self):
+        return self.name
 
 class Projects(models.Model):
     title = models.CharField(max_length=50)
@@ -12,3 +15,6 @@ class Projects(models.Model):
     start_commit = models.DateField()
     last_commit = models.DateField()
     finished = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
