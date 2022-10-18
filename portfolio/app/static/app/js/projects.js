420b8f16c11600f96bfea33740aed35d4a7e5f0a
sserve-kr <personal@sserve.me>
2022-10-18 17:59:51 +0900

    Applied swipe for phone and tablet in projects

--- portfolio/app/static/app/js/projects.js
+++ portfolio/app/static/app/js/projects.js
@@ -1,25 +1,61 @@
+function nextMove() {
+    var current = document.querySelector('.current.project');
+    var next = current.nextElementSibling;
+    if (next) {
+        next.classList.remove("down");
+        next.classList.add("current");
+        current.classList.remove("current");
+        current.classList.add("up");
+    }
+}
+
+function prevMove() {
+    var current = document.querySelector('.current.project');
+    var previous = current.previousElementSibling;
+    if (previous) {
+        previous.classList.remove("up");
+        previous.classList.add("current");
+        current.classList.remove("current");
+        current.classList.add("down");
+    }
+}
+
 document.addEventListener('wheel', function(e) {
     if (e.deltaY > 0) {
-        var current = document.querySelector('.current.project');
-        var next = current.nextElementSibling;
-        if (next) {
-            next.classList.remove("down");
-            next.classList.add("current");
-            current.classList.remove("current");
-            current.classList.add("up");
-        }
+        nextMove();
     }
     else if (e.deltaY < 0) {
-        var current = document.querySelector('.current.project');
-        var previous = current.previousElementSibling;
-        if (previous) {
-            previous.classList.remove("up");
-            previous.classList.add("current");
-            current.classList.remove("current");
-            current.classList.add("down");
-        }
+        prevMove();
     }
 });
+
+mainElement = document.querySelector('main');
+
+mainElement.addEventListener("mousedown", (e) => {
+    startPoint = e.pageY;
+});
+
+mainElement.addEventListener("mouseup", (e) => {
+    endPoint = e.pageY;
+    if (startPoint < endPoint) {
+        prevMove();
+    } else if (startPoint > endPoint) {
+        nextMove();
+    }
+});
+
+mainElement.addEventListener('touchstart', (e) => {
+    startPoint = e.touches[0].pageY;
+});
+
+mainElement.addEventListener('touchend', (e) => {
+    endPoint = e.changedTouches[0].pageX;
+    if (startPoint < endPoint) {
+        prevMove();
+    } else if (startPoint > endPoint) {
+        nextMove();
+    }
+})
 
 document.querySelector('.projects').children[0].classList.add("current");
 document.querySelector('.projects').children[0].classList.remove("down");
