143ffa92c481f231770021cbb0465a612d3a9774
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-11-05 01:36:40 +0000

    Changed project model

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class ProjectDependency(models.Model):
-    name = models.CharField(max_length=20, unique=True, primary_key=True)
-    icon = models.ImageField(upload_to='icons')
-    
-    def __str__(self):
-        return self.name
+from markdown2 import markdown
 
 class Project(models.Model):
-    title = models.CharField(max_length=50)
+    href = models.CharField(max_length=20, unique=True, default='')
+    title = models.CharField(max_length=20)
     short_description = models.CharField(max_length=100)
-    dependencies = models.ManyToManyField(ProjectDependency)
+    main_content = models.TextField(default="full description with **[markdown](https://commonmark.org)**")
+    content_as_markdown = models.TextField(editable=False, default="")
     start_commit = models.DateField()
     last_commit = models.DateField()
-    finished = models.BooleanField(default=False)
-    
+
     def __str__(self):
-        return self.title
+        return str(self.title)
+
+    def save(self, *args, **kwargs):
+        self.content_as_markdown = markdown(self.main_content, extras=[
+            "footnotes",
+            "nofollow",
+            "target-blank-links",
+            "strike"
+        ])
+        super(Project, self).save(*args, **kwargs)
