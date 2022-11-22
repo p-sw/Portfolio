6bf0673057a3fde9de21d1886ed57c7ff0477431
sserve-kr <personal@sserve.me>
2022-11-22 18:14:32 +0900

    Removed contents

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -1,34 +1,21 @@
 from django.db import models
 
-from markdown2 import markdown
 
 class Project(models.Model):
     href = models.CharField(max_length=20, unique=True, default='')
     title = models.CharField(max_length=20)
     short_description = models.CharField(max_length=100)
-    main_content = models.TextField(default="full description with **[markdown](https://commonmark.org)**")
-    content_as_markdown = models.TextField(editable=False, default="")
     start_commit = models.DateField()
     last_commit = models.DateField()
 
     def __str__(self):
         return str(self.title)
 
-    def save(self, *args, **kwargs):
-        self.content_as_markdown = markdown(self.main_content, extras=[
-            "footnotes",
-            "nofollow",
-            "target-blank-links",
-            "strike"
-        ])
-        super(Project, self).save(*args, **kwargs)
-    
     def as_dictionary(self):
         return {
             "href": self.href,
             "title": self.title,
             "short_description": self.short_description,
-            "content": self.content_as_markdown,
             "start_commit": self.start_commit.strftime("%Y-%m-%d"),
             "last_commit": self.last_commit.strftime("%Y-%m-%d")
         }
